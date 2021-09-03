53b69d6771f0c08f96b6c5887590b7d234d6173e
Leonardo Portes <leol_portes@hotmail.com>
2021-09-02 23:02:44 -0300

    Accordion refatotorado para class

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import SoftScroll from './modules/soft-scroll.js';
-import initAccordion from './modules/accordion.js';
+import Accordion from './modules/accordion.js';
 import initTabNavigation from './modules/tabnav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
@@ -13,7 +13,9 @@ import initscrollAnimation from './modules/scroll-animation.js';
 const softScroll = new SoftScroll('[data-menu="soft"] a[href^="#"]');
 softScroll.init();
 
-initAccordion();
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
+
 initTabNavigation();
 initModal();
 initTooltip();
